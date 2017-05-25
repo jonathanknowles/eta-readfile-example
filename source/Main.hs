43024728a84b5c5324649220d52e613502e11b4f
Jonathan Knowles <mail@jonathanknowles.net>
2017-05-25 14:01:19 +0800

    Add conversion from JByteArray to ByteString.

--- source/Main.hs
+++ source/Main.hs
@@ -2,8 +2,11 @@
 
 module Main where
 
+import Data.ByteString (ByteString)
 import Java
 import System.IO (BufferMode (..), hSetBuffering, stdout)
+
+import qualified Data.ByteString as B
 
 data {-# CLASS "java.net.URI" #-} URI = URI (Object# URI)
     deriving (Class, Show)
@@ -20,16 +23,15 @@ foreign import java unsafe "@static java.nio.file.Paths.get"
 foreign import java unsafe "@static java.nio.file.Files.readAllBytes"
     readAllBytes :: Path -> IO JByteArray
 
+toByteString :: JByteArray -> ByteString
+toByteString ba = B.pack $ fromIntegral <$> bs
+    where bs = fromJava ba :: [Byte]
+
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
     putStrLn "Please enter a valid file URI:"
-    uriText <- getLine
-    uri <- createURI uriText
-    path <- getPath uri
-    content <- readAllBytes path
-    -- content has type JByteArray
-    length <- javaWith content alength
+    bytes <- toByteString <$> (readAllBytes =<< getPath =<< createURI =<< getLine)
     putStrLn "Number of bytes read:"
-    putStrLn $ show length
+    putStrLn $ show $ B.length bytes
 
